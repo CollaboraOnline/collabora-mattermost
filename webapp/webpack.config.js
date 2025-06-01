c1bb25e0d232e89dfdc7e5baf7f47cba270ad0ff
Hubert Figuière <hub@collabora.com>
2025-05-31 23:22:44 -0400

    webpack: Fix resolve.extensions
    Signed-off-by: Hubert Figuière <hub@collabora.com>

--- webapp/webpack.config.js
+++ webapp/webpack.config.js
@@ -44,7 +44,7 @@ module.exports = {
             'node_modules',
             path.resolve(__dirname),
         ],
-        extensions: ['*', '.js', '.jsx', '.ts', '.tsx'],
+        extensions: ['.js', '.jsx', '.ts', '.tsx'],
     },
     module: {
         rules: [
